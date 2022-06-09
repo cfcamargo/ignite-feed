ef5c37830c433367f37f5183a615c7221b059408
Cristian Camargo <chris.camargo2015@gmail.com>
2022-06-09 18:33:53 -0400

    Create and style sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { Header } from './components/Header'
 import './components/global.css'
 
+import styles from '../src/components/App.module.css'
+import { Sidebar } from './components/sidebar'
+  
 
 function App() {
- 
+
 
   return (
-    <Header />
+    <>
+      <Header />
+
+        <div className={styles.wrapper}>
+
+           <Sidebar />
+        
+        <main>
+          <h2>post</h2>
+        </main>
+
+      </div>
+    </>
   )
 }
 
